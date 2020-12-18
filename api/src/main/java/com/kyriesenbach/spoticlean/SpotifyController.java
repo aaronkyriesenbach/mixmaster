4507babababad4057e18cde6af4329adb04c1eff
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2020-12-17 21:44:15 -0500

    Flesch out authorization, add playlist methods

--- api/src/main/java/com/kyriesenbach/spoticlean/SpotifyController.java
+++ api/src/main/java/com/kyriesenbach/spoticlean/SpotifyController.java
@@ -1,13 +1,18 @@
 package com.kyriesenbach.spoticlean;
 
-import com.kyriesenbach.spoticlean.exceptions.PlaylistNotFoundException;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController("/spotify")
@@ -24,18 +29,30 @@ public class SpotifyController
     @GetMapping("/authorize")
     public URI authorize()
     {
-        return spotifyService.authorize();
+        return spotifyService.getAuthURL();
     }
     
-    @PostMapping("/authorize")
-    public void setAccessToken(@RequestParam String token)
+    @GetMapping("/accesstoken")
+    public String getAccessToken(@RequestParam String authorizationCode) throws SpotifyWebApiException, ParseException, IOException
     {
-        spotifyService.setAccessToken(token);
+        return spotifyService.getAccessToken(authorizationCode);
     }
     
     @GetMapping("/playlist")
-    public Playlist getPlaylist(@RequestParam String id) throws PlaylistNotFoundException
+    public Playlist getPlaylist(@RequestHeader(name = "Authorization") String accessToken, @RequestParam String id) throws SpotifyWebApiException, ParseException, IOException
     {
-        return spotifyService.getPlaylist(id);
+        return spotifyService.getPlaylist(accessToken, id);
+    }
+    
+    @GetMapping("/playlist/current")
+    public Paging<PlaylistSimplified> getCurrentUserPlaylists(@RequestHeader(name = "Authorization") String accessToken) throws ParseException, SpotifyWebApiException, IOException
+    {
+        return spotifyService.getCurrentUserPlaylists(accessToken);
+    }
+    
+    @PostMapping("/playlist/create")
+    public Playlist createPlaylist(@RequestHeader(name = "Authorization") String accessToken, @RequestParam String name) throws ParseException, SpotifyWebApiException, IOException
+    {
+        return spotifyService.createPlaylist(accessToken, name, true);
     }
 }
--- api/src/main/java/com/kyriesenbach/spoticlean/SpotifyService.java
+++ api/src/main/java/com/kyriesenbach/spoticlean/SpotifyService.java
@@ -1,13 +1,23 @@
 package com.kyriesenbach.spoticlean;
 
-import com.kyriesenbach.spoticlean.exceptions.PlaylistNotFoundException;
 import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.specification.Paging;
 import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import com.wrapper.spotify.model_objects.specification.User;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import com.wrapper.spotify.requests.data.playlists.CreatePlaylistRequest;
+import com.wrapper.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
 import com.wrapper.spotify.requests.data.playlists.GetPlaylistRequest;
+import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
 
 @Service
@@ -20,28 +30,45 @@ public class SpotifyService
         .setRedirectUri(URI.create("http://localhost"))
         .build();
     
-    public URI authorize()
+    public URI getAuthURL()
     {
-        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().build();
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().scope("playlist-modify-public, playlist-modify-private").build();
         return authorizationCodeUriRequest.execute();
     }
     
-    public void setAccessToken(String token)
+    public String getAccessToken(String authorizationCode) throws SpotifyWebApiException, ParseException, IOException
     {
-        spotifyApi.setAccessToken(token);
-        log.info("Successfully set access token " + token);
+        final AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(authorizationCode).build();
+        final AuthorizationCodeCredentials authorizationCodeCredentials;
+        authorizationCodeCredentials = authorizationCodeRequest.execute();
+        log.info("Access token expires in: " + authorizationCodeCredentials.getExpiresIn());
+        return authorizationCodeCredentials.getAccessToken();
     }
     
-    public Playlist getPlaylist(String id) throws PlaylistNotFoundException
+    public Playlist getPlaylist(String accessToken, String id) throws SpotifyWebApiException, ParseException, IOException
     {
+        spotifyApi.setAccessToken(accessToken);
         GetPlaylistRequest getPlaylistRequest = spotifyApi.getPlaylist(id).build();
-        try
-        {
-            return getPlaylistRequest.execute();
-        }
-        catch (Exception e)
-        {
-            throw new PlaylistNotFoundException(id);
-        }
+        return getPlaylistRequest.execute();
+    }
+    
+    public Paging<PlaylistSimplified> getCurrentUserPlaylists(String accessToken) throws ParseException, SpotifyWebApiException, IOException
+    {
+        spotifyApi.setAccessToken(accessToken);
+        final GetListOfCurrentUsersPlaylistsRequest getListOfCurrentUsersPlaylistsRequest = spotifyApi.getListOfCurrentUsersPlaylists().build();
+        return getListOfCurrentUsersPlaylistsRequest.execute();
+    }
+    
+    private User getCurrentUser() throws ParseException, SpotifyWebApiException, IOException
+    {
+        final GetCurrentUsersProfileRequest getCurrentUsersProfileRequest = spotifyApi.getCurrentUsersProfile().build();
+        return getCurrentUsersProfileRequest.execute();
+    }
+    
+    public Playlist createPlaylist(String accessToken, String name, boolean public_) throws ParseException, SpotifyWebApiException, IOException
+    {
+        spotifyApi.setAccessToken(accessToken);
+        final CreatePlaylistRequest createPlaylistRequest = spotifyApi.createPlaylist(getCurrentUser().getId(), name).public_(public_).build();
+        return createPlaylistRequest.execute();
     }
 }
