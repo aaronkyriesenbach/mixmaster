d3962e85e6dbd0915d477a88fdec52a264b624fd
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2021-03-03 13:13:28 -0500

    Use correct API url

--- ui/src/constants.tsx
+++ ui/src/constants.tsx
@@ -1,2 +1,2 @@
-export const BASE_API_URL = process.env.NODE_ENV === 'development' ? (process.env.REACT_APP_API_HOST ?? 'http://localhost:5000') : 'https://mixster.io';
+export const BASE_API_URL = process.env.NODE_ENV === 'development' ? (process.env.REACT_APP_API_HOST ?? 'http://localhost:5000') : 'https://api.mixster.io';
 export const SPOTIFY_TOKEN = 'spotify_token';
